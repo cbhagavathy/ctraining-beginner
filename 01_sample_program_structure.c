a1113cef94b0ff4c4d345cf2ec5ab94f8d7142dc
cbhagavathy <85443214+cbhagavathy@users.noreply.github.com>
2022-12-19 10:25:55 +0530

    Create 01_sample_program_structure.c

---
+++ 01_sample_program_structure.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//Global Variables
+int x = 5;
+
+int main() {
+	int x = 10;
+
+	printf("Welcome to C\n");
+
+	printf("Local x : %d\n", x);
+
+	//Sub block to create a separate scope
+	//and access global variable
+	{
+		extern int x;
+		printf("Global x : %d\n", x);
+	}
+
+	return 0;
+}
