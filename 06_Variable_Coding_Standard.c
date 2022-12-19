8bd2b60996b5a49740aa64e7e1721aecce6dfbaa
cbhagavathy <85443214+cbhagavathy@users.noreply.github.com>
2022-12-19 10:34:31 +0530

    Create 06_Variable_Coding_Standard.c

---
+++ 06_Variable_Coding_Standard.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*Try using Hungarian notation. The notation is an attempt to formalize how a variable’s name can indicate its type or purpose.*/
+
+int main() {
+	char *sObjectName = "Circle";		//s indicates string 
+	int  iNoOfObjects = 40;				//i indicates integer 
+	float fRadius = 10.0; 				//f indicates float
+	return 0;
+}
