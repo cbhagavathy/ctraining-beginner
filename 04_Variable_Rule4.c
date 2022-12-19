a9a1b1470df08d9ce253d31936cd20f8139a3ef0
cbhagavathy <85443214+cbhagavathy@users.noreply.github.com>
2022-12-19 10:29:46 +0530

    Create 04_Variable_Rule4.c

---
+++ 04_Variable_Rule4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*Variable names are case sensitive.*/
+
+int main() {
+	int a=10, A=20;
+	printf("Hello, World!\n");
+	printf("a : %d\n", a);
+	printf("A : %d\n", A);
+	return 0;
+}
