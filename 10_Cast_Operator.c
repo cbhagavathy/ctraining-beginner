5710d46d5997109566c461f6f3ded57e149ecdae
cbhagavathy <85443214+cbhagavathy@users.noreply.github.com>
2022-12-19 11:24:34 +0530

    Create 10_Cast_Operator.c

---
+++ 10_Cast_Operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*Used to explicitly convert the data type from one data type to another. Conversion process is called Type Casting.*/
+
+int main()
+{
+    printf("PI : %f\n", 22/7); 
+    printf("PI : %f\n", (float)22/7); 
+    return 0;
+}
+
