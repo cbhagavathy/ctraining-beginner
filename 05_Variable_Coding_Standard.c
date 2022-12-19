b90c336ea3ef6c915742890b1c38e46241f9ed2c
cbhagavathy <85443214+cbhagavathy@users.noreply.github.com>
2022-12-19 10:31:14 +0530

    Create 05_Variable_Coding_Standard.c

---
+++ 05_Variable_Coding_Standard.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*As a standard we can use either CamelCase or PascalCase or SnakeCase.*/
+
+int main() {
+	char *firstName = "Chidhambaram"; 	//Camel Case
+	char *LastName = "Bhagavathy"; 		//Pascal Case
+	char *first_name = "Chidhambaram";	//Snake Case
+	return 0;
+}
