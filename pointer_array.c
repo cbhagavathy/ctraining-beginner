eff007f393c59128eda1ecd2b272ae226edb6209
cbhagavathy <85443214+cbhagavathy@users.noreply.github.com>
2022-12-27 23:10:00 +0530

    Update pointer_array.c

--- pointer_array.c
+++ pointer_array.c
@@ -2,35 +2,40 @@
 
 int main() {
     int i, a[10] = {1,2,3,4,5,6,7,8,9,10};
-    int (*ptr_a)[10] = &a; //pointer to array of 10 integer 
-    int *ptr_aa = a; //pointer to integer 
-    
+    int (*ptr_a)[10] = &a; //pointer to array of 10 integer
+    int *ptr_aa = a; //pointer to integer
+
     printf("Displaying data from Array (a) : \n");
     for(i=0; i<10; i++) {
         printf("-> %d ", a[i]);
     }
     printf("\n");
-    printf("Displaying data from pointer to a (ptr_a) : \n");
+    printf("Displaying data from pointer to a (ptr_a) : -- (*ptr_a)[i] \n");
     for(i=0; i<10; i++) {
         printf("-> %d ", (*ptr_a)[i]);
     }
     printf("\n");
-    printf("Displaying data from pointer to a (ptr_aa) : \n");
+    printf("Displaying data from pointer to a (ptr_aa) : -- ptr_aa[i] \n");
     for(i=0; i<10; i++) {
         printf("-> %d ", ptr_aa[i]);
     }
     printf("\n");
-    printf("Displaying data from pointer to a (ptr_aa) : \n");
+    printf("Displaying data from pointer to a (ptr_aa) : -- i[ptr_aa] \n");
+    for(i=0; i<10; i++) {
+        printf("-> %d ", i[ptr_aa]);
+    }
+    printf("\n");
+    printf("Displaying data from pointer to a (ptr_aa) : -- *(ptr_aa+i) \n");
     for(i=0; i<10; i++) {
         printf("-> %d ",*(ptr_aa+i));
     }
     printf("\n");
-    printf("Displaying data from pointer to a (ptr_aa) : \n");
+    printf("Displaying data from pointer to a (ptr_aa) : -- *ptr_aa++ \n");
     for(i=0; i<10; i++) {
         printf("-> %d ", *ptr_aa++);
     }
     printf("\n");
-    
+
     return 0;
 }
 
