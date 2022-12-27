9b0de96fd3ded5f96609650e348af904345d9a0b
cbhagavathy <85443214+cbhagavathy@users.noreply.github.com>
2022-12-27 22:37:25 +0530

    Create pointer_array.c

---
+++ pointer_array.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main() {
+    int i, a[10] = {1,2,3,4,5,6,7,8,9,10};
+    int (*ptr_a)[10] = &a; //pointer to array of 10 integer 
+    int *ptr_aa = a; //pointer to integer 
+    
+    printf("Displaying data from Array (a) : \n");
+    for(i=0; i<10; i++) {
+        printf("-> %d ", a[i]);
+    }
+    printf("\n");
+    printf("Displaying data from pointer to a (ptr_a) : \n");
+    for(i=0; i<10; i++) {
+        printf("-> %d ", (*ptr_a)[i]);
+    }
+    printf("\n");
+    printf("Displaying data from pointer to a (ptr_aa) : \n");
+    for(i=0; i<10; i++) {
+        printf("-> %d ", ptr_aa[i]);
+    }
+    printf("\n");
+    printf("Displaying data from pointer to a (ptr_aa) : \n");
+    for(i=0; i<10; i++) {
+        printf("-> %d ", *ptr_aa++);
+    }
+    printf("\n");
+    
+    return 0;
+}
+
+
